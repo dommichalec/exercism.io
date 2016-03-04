759c4f443b4380d8d54a437e13f7825abdfa1972
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-03-04 14:32:18 -0500

    finish/refactor acronym

---
+++ acronym/acronym.rb
@@ -0,0 +1,18 @@
+ACRONYM_PAIRS = {
+  'Portable Network Graphics' => 'PNG',
+  'Ruby on Rails' => 'ROR',
+  'HyperText Markup Language' => 'HTML',
+  'First In, First Out' => 'FIFO',
+  'PHP: Hypertext Preprocessor' => 'PHP',
+  'Complementary metal-oxide semiconductor' => 'CMOS'
+ }.freeze
+
+# Acronym class
+class Acronym
+  VERSION = 1
+  
+  def self.abbreviate(given)
+    expected = ACRONYM_PAIRS[given]
+    expected
+  end
+end
